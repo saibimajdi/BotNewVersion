f9bba434c2668677e3a0b7976c32150255adbe32
Saibi Majdi <saibimajdi@outlook.com>
2017-04-10 21:29:48 +0200

    hub

---
+++ Hubs/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MyBot.Hubs
+{
+    public class MyHub : Hub
+    {
+        public void SendMessageTo(string message, string user)
+        {
+            Clients.All.Notify(message, user);
+        }
+    }
+}
